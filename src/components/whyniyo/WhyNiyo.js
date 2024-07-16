3e1495ef6f7c1afca580ad0d48cb5e240ff6ecc9
oyinkan <skusman@student.lautech.edu.ng>
2024-07-16 21:38:19 +0100

    worked on the WhyNiyo component

---
+++ src/components/whyniyo/WhyNiyo.js
@@ -0,0 +1,14 @@
+import "./WhyNiyo.css";
+
+function WhyNiyo() {
+  return (
+    <section id="why-niyo">
+      <p class="why-text">Why Niyo Bootcamp</p>
+      <h2>
+        Bespoke all around <br /> <span>Support</span>
+      </h2>
+    </section>
+  );
+}
+
+export default WhyNiyo;
